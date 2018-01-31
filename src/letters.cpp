bdee5a9225cf69612892eb0dd426daead5d85f39
msloma144 <syphr144@gmail.com>
2018-01-30 19:02:26 -0500

    Added letters test

---
+++ src/letters.cpp
@@ -0,0 +1,16 @@
+bool A[][] = [
+[true, false, true],
+[true, false, true],
+[true, false, true],
+[true, false, true],
+[true, false, true]
+];
+
+public:
+  bool getLetterArray();
+
+bool getLetterArray(char letter){
+  switch letter:
+  case 'A': return A
+  default : return false;
+}
