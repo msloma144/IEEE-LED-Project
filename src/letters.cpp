901ee6c5c19d3a4f88b3b3e4d9816923bcba3c26
msloma144 <syphr144@gmail.com>
2018-02-01 17:16:17 -0500

    Added more letters
    Added more letters and changed to vectors for letter storage

--- src/letters.cpp
+++ src/letters.cpp
@@ -1,16 +1,87 @@
-bool A[][] = [
-[true, false, true],
-[true, false, true],
-[true, false, true],
-[true, false, true],
-[true, false, true]
-];
+#include <string>
+#include<vector>
 
-public:
-  bool getLetterArray();
+std::vector<std::vector<bool>>  A1{
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 1}
+};
 
-bool getLetterArray(char letter){
-  switch letter:
-  case 'A': return A
+std::vector<std::vector<bool>>  B{
+        {1, 1, 0},
+        {1, 0, 1},
+        {1, 1, 0},
+        {1, 0, 1},
+        {1, 1, 0}
+};
+
+std::vector<std::vector<bool>>  C{
+        {0, 1, 1, 1},
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 1, 1}
+};
+
+std::vector<std::vector<bool>>  D{
+        {1, 1, 0},
+        {1, 0, 1},
+        {1, 0, 1},
+        {1, 0, 1},
+        {1, 1, 0}
+};
+
+std::vector<std::vector<bool>>  E{
+        {1, 1, 1},
+        {1, 0, 0},
+        {1, 1, 1},
+        {1, 0, 0},
+        {1, 1, 1}
+};
+
+std::vector<std::vector<bool>>  F{
+        {1, 1, 1},
+        {1, 0, 0},
+        {1, 1, 1},
+        {1, 0, 0},
+        {1, 0, 0}
+};
+
+std::vector<std::vector<bool>>  G{
+       {0, 1, 1, 1, 0},
+       {1, 1, 0, 0, 0},
+       {1, 1, 0, 1, 1},
+       {1, 1, 0, 0, 1},
+       {0, 1, 1, 1, 0}
+};
+
+std::vector<std::vector<bool>>  H{
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {1, 1, 1, 1},
+        {1, 0, 0, 1},
+        {1, 1, 0, 1}
+};
+
+
+//protos
+bool getLetterArray(char);
+void returnFormmattedArr(std::string, int, int);
+
+
+std::vector<std::vector<bool>> getLetterArray(char letter){
+  switch (letter) {
+  case 'A': return A;
+  case 'B': return B;
+  case 'C': return C;
+  case 'D': return D;
+
   default : return false;
+  }
+}
+
+void returnFormmattedArr(std::string input, int lineLength, int NUM_LEDS){
+    //FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
 }
