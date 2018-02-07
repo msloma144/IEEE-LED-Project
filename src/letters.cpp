5676120e430464d6b8c78ff9cb519356ebb7ca35
msloma144 <syphr144@gmail.com>
2018-02-07 13:31:21 -0500

    Update
    Fixed some problems, made more

--- src/letters.cpp
+++ src/letters.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "lettersLib.cpp"
 #include <FastLED.h>
+#include <ctype.h>
 
 
 //protos
@@ -58,11 +59,54 @@ void processCharLine(std::vector<bool> line){
   }
 }
 
+char* inputToChar(std::string input, int inputLength){
+  char charInput[inputLength + 1];
+  return strcpy(charInput, input.c_str());
+}
+
+void charToMatrix(char inputArr[]){
+  std::vector<std::vector<bool>> matrix;
+  int inputLength = *(&inputArr + 1) - inputArr;
+
+  for(int i = 0; i < 5; i++){
+    for(int j = 0; j < inputLength; j++){
+      //add the letter matrix part for the current line
+      matrix.at(i).push_back(getLetterArray(inputArr[j]).at(i))
+
+      //add small space if current char AND next char are alpha
+      if(isalpha(inputArr[j]) && isalpha(inputArr[j+1])){
+        matrix.at(i).push_back(getLetterArray('_').at(i))
+      }
+    }
+  }
+  //return matrix;
+}
+
+void setColors(std::vector<std::vector<bool>> matrix){
+  std::vector<std::vector<CRGB>> colorMatrix;
+  //initialize colorMatix to same size as martix
+  for(int i = 0; i < matrix.size(); i++){
+    for(int j = 0; j < matrix.at(0).size(); j++){
+      if(matrix.at(i).at(j)){
+        colorMatrix.at(i).assign(j , CRGB::Black);
+      }
+      else{
+        colorMatrix.at(i).assign(j , CRGB::Black);
+      }
+    }
+  }
+}
+void matrixToArr(){
+  //turns vector<vector<CRGB>> into CRGB 1d array
+}
+void slideView(){
+  //returns CRGB 1d array
+}
+
 void returnFormmattedArr(std::string input, int LINE_LENGTH, int NUM_LEDS, CRGB &leds){
     // string to char array
     int inputLength = input.length();
-    char charInput[inputLength + 1];
-    strcpy(charInput, input.c_str());
+    char *charInput = inputToChar(input, input.length());
 
     int NUM_LINES = NUM_LEDS / LINE_LENGTH;
 
