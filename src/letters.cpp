389f93faad5844cf1fe9ca487fe7cbedffaacb97
msloma144 <syphr144@gmail.com>
2018-02-18 19:50:40 -0500

    Update

--- src/letters.cpp
+++ src/letters.cpp
@@ -3,6 +3,7 @@
 #include "lettersLib.h"
 #include <FastLED.h>
 #include <ctype.h>
+#include <cstring>
 
 
 //protos
@@ -68,41 +69,36 @@ char* inputToChar(std::string input){
   return strcpy(charInput, input.c_str());
 }
 
-std::vector<std::vector<bool>> charToMatrix(char inputArr[], int LINE_LENGTH){
+std::vector<std::vector<bool>> charToMatrix(char inputArr[], unsigned int inputLength, unsigned int LINE_LENGTH){
   //takes in the array of chars (input) and converts them into
   //a boolean matrix as prescribed by their corresponding letter matrix
   //in lettersLib
 
   std::vector<std::vector<bool>> matrix(5);
-  int inputLength = *(&inputArr + 1) - inputArr;
 
-  for(int i = 0; i < 5; i++){
-    matrix.at(i) = std::vector<bool>(LINE_LENGTH);
+  for(unsigned int i = 0; i < 5; i++){
 
-    for(int j = 0; j < inputLength; j++){
+
+    for(unsigned int j = 0; j < inputLength; j++){
       //add the letter matrix part for the current line
       std::vector<bool> temp = getLetterArray(inputArr[j]).at(i);
-      std::vector<bool> line = matrix.at(i); //might work
-      line.insert(line.end(), temp.begin(), temp.end());
+      matrix.at(i).insert(matrix.at(i).end(), temp.begin(), temp.end());
 
       //add small space if current char AND next char are alpha
       if(isalpha(inputArr[j]) && isalpha(inputArr[j+1])){
-        std::vector<bool> temp = getLetterArray('_').at(i);
-        std::vector<bool> line = matrix.at(i); //might work
-        line.insert(line.end(), temp.begin(), temp.end());
+        temp = getLetterArray('_').at(i);
+          matrix.at(i).insert(matrix.at(i).end(), temp.begin(), temp.end());
       }
     }
   }
+
   while((matrix.at(0).size() % LINE_LENGTH) != 0){
     //add spaces to the end of the line till the matrix has a moduluo many columns as
     //there are pixels
     for(unsigned int i = 0; i < matrix.size(); i++){
-      for (size_t j = 0; j < matrix.at(0).size(); j++) {
-        matrix.at(i).push_back(0);
-      }
+      matrix.at(i).push_back(0);
     }
   }
-
   return matrix;
 }
 
@@ -110,14 +106,14 @@ std::vector<std::vector<CRGB>> setColors(std::vector<std::vector<bool>> matrix){
   std::vector<std::vector<CRGB>> colorMatrix(matrix.size());
   //initialize colorMatix to same size as martix
   for(unsigned int i = 0; i < matrix.size(); i++){
-    colorMatrix.at(i) = std::vector<CRGB>(matrix.at(0).size());
+    colorMatrix.at(i) = std::vector<CRGB>();
 
     for(unsigned int j = 0; j < matrix.at(0).size(); j++){
       if(matrix.at(i).at(j)){
-        colorMatrix.at(i).assign(j , CRGB::Red);
+        colorMatrix.at(i).push_back(CRGB::Red);
       }
       else{
-        colorMatrix.at(i).assign(j , CRGB::Black);
+        colorMatrix.at(i).push_back(CRGB::Black);
       }
     }
   }
@@ -144,26 +140,35 @@ CRGB* matrixToArr(std::vector<std::vector<CRGB>> colorMatrix){
 
 std::vector<std::vector<CRGB>> stringtoColor(std::string input, int LINE_LENGTH, int NUM_LEDS){
     //int NUM_LINES = NUM_LEDS / LINE_LENGTH;
-
     char* charInput;
     charInput = inputToChar(input);
+    unsigned int inputLength = input.length();
 
-    std::vector<std::vector<bool>> boolMatrix = charToMatrix(charInput, LINE_LENGTH);
+    std::vector<std::vector<bool>> boolMatrix = charToMatrix(charInput, inputLength, LINE_LENGTH);
     std::vector<std::vector<CRGB>> colorMatrix = setColors(boolMatrix);
 
     return colorMatrix;
 }
 
 CRGB* slideView(std::vector<std::vector<CRGB>> colorMatrix, unsigned int LINE_LENGTH, unsigned int count){
+  Serial.println("Entered...");
   //takes in colorMatrix and returns CRGB 1d array
   std::vector<std::vector<CRGB>> outputMatrix(colorMatrix.size());
 
   for(unsigned int i = 0; i < colorMatrix.size(); i++){
     //get subsection of vector for output
-    outputMatrix.at(i) = std::vector<CRGB>(colorMatrix.at(i).begin() + count, colorMatrix.at(i).begin() + (LINE_LENGTH + count));
+    outputMatrix.at(i) = std::vector<CRGB>();
+
+    for(unsigned int j = count; j < LINE_LENGTH + count; j++){
+      outputMatrix.at(i).push_back(colorMatrix.at(i).at(j));
+
+    }
+    Serial.println(i);
   }
 
+  Serial.println("Shifted...");
   CRGB *output;
   output = matrixToArr(outputMatrix);
+  Serial.println("PREP...");
   return output;
 }
