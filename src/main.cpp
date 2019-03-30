de132845cdd3be112c1051d0e09985fd3adae3f8
msloma144 <syphr144@gmail.com>
2019-03-29 21:34:24 -0400

    Most fixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,48 @@
 
 using namespace std;
 
+
 int get_max_length(char word[], int line_width) {
-    int max_length = 0;
+    int max_line_length = 0;
     int content_length = 0;
     int line_pos;
 
     for (int index = 0; word[index] != '\0'; index++) {
         content_length += getLetterLength(word[index]);
+
+        // account for small spaces
+        if(word[index] != "_" | word[index] != "@" | word[index] != "."){
+          content_length += getLetterLength("");
+        }
     }
 
     line_pos = 0;
 
+    // iter through each letter in string
     for (int index = 0; word[index] != '\0'; index++) {
         int letter_len = getLetterLength(word[index]);
 
+        // add letter length
         for (int j = 0; j < letter_len; j++) {
             line_pos++;
-            max_length++;
+            max_line_length++;
+        }
+        // account for small spaces between letters
+        if(word[index] != "_" | word[index] != "@" | word[index] != "."){
+          line_pos++;
+          max_line_length++;
         }
     }
 
+    // add length of sign at end for blank trailing string
     if (line_pos < (content_length + line_width)) {
         for (int j = line_pos; j < (content_length + line_width); j++) {
-            max_length++;
+            max_line_length++;
         }
     }
+    Serial.println(max_line_length);
 
-    return max_length;
+    return max_line_length;
 }
 
 
@@ -42,57 +57,111 @@ bool* form_matrix(char word[], int line_width, int num_lines) {
     int line_pos;
     int max_positons;
     bool *output;
+    int raw_letter_string_len;
 
     // get content width
     for (int index = 0; word[index] != '\0'; index++) {
         content_length += getLetterLength(word[index]);
+
+        // account for small spaces
+        if(word[index] != "_" | word[index] != "@" | word[index] != "."){
+          content_length += getLetterLength("");
+        }
+        raw_letter_string_len += 1;
     }
 
     max_positons = (content_length + line_width) * num_lines;
     output = new bool[max_positons];
 
     // build matrix
-    for (int i = 0; i < num_lines; i++) {
+    for (int line_num = 0; line_num < num_lines; line_num++) {
         line_pos = 0;
 
         // fills line with letters
-        for (int index = 0; word[index] != '\0'; index++) {
-            bool *x = getLetterArray(word[index], i);
+        for (int index = 0; index < raw_letter_string_len; index++) {
+            bool *x = getLetterArray(word[index], line_num);
             int letter_len = getLetterLength(word[index]);
 
+            // add letter for this line
             for (int j = 0; j < letter_len; j++) {
                 output[curr_length] = x[j];
                 curr_length++;
                 line_pos++;
 
-                if (i == 0) {
+                if (line_num == 0) {
                     max_length++;
                 }
             }
+
+            // add small space between letters
+            if(word[index] != "_" | word[index] != "@" | word[index] != "."){
+              bool *x = getLetterArray("", line_num);
+              int letter_len = getLetterLength("");
+
+              for (int j = 0; j < letter_len; j++) {
+                  output[curr_length] = x[j];
+                  curr_length++;
+                  line_pos++;
+
+                  if (line_num == 0) {
+                      max_length++;
+                  }
+              }
+            }
         }
+        //Serial.println(line_pos);
 
         // if space still remains on line, fill with spaces
         if (line_pos < (content_length + line_width)) {
             for (int j = line_pos; j < (content_length + line_width); j++) {
                 output[curr_length] = 0;
                 curr_length++;
+                line_pos++;
 
-                if (i == 0) {
+                if (line_num == 0) {
                     max_length++;
                 }
             }
         }
+        //Serial.println(line_pos);
+
+        if(line_num == 0){
+          for(int b = 0; b < content_length + line_width; b++){
+            Serial.print(output[b]);
+            Serial.print(" ");
+          }
+        }
+        else{
+          for(int b = (content_length + line_width) * line_num; b < (content_length + line_width) * (line_num + 1); b++){
+            Serial.print(output[b]);
+            Serial.print(" ");
+          }
+        }
+        Serial.println();
     }
+    //***************************
+    //for (int i = 0; i < num_lines; i++){
+    //  if(i == 0){
+    //    for(int b = 0; b < content_length + line_width; b++){
+    //      Serial.print(output[b]);
+    //      Serial.print(" ");
+    //    }
+    //  }
+    //  else{
+    //    for(int b = (content_length + line_width) * i; b < (content_length + line_width) * (i + 1); b++){
+    //      Serial.print(output[b]);
+    //      Serial.print(" ");
+    //    }
+    //  }
+    //  Serial.println();
+    //}
 
     return output;
 }
 
 
-void scroll(bool* window, int line_width, int num_lines, int max_length, bool output[],
+void scroll(bool* window, int line_width, int num_lines, int max_length, bool* output,
             int start, int end) {
-
-    //bool *window = new bool[num_lines * line_width];
-
     int index = 0;
     for (int j = 0; j < num_lines; j++) {
         if (start < end) {
@@ -116,8 +185,6 @@ void scroll(bool* window, int line_width, int num_lines, int max_length, bool ou
             }
         }
     }
-
-    //return window;
 }
 
 
@@ -127,53 +194,118 @@ void scroll(bool* window, int line_width, int num_lines, int max_length, bool ou
 #define BRIGHTNESS  64
 #define UPDATES_PER_SECOND 100
 
-#define num_leds 29 * 5
-int num_lines = 5;
+#define num_lines 10
+#define num_leds 28 * num_lines
+
 int line_width = num_leds / num_lines;
 
 CRGB leds[num_leds];
 
-int max_iter = 10000;
-int i = 0;
-int start = 0;
-int end = line_width;
+// start line two at an offset so it doesn't line up
+// and look confusing
+int start_one = 0;
+int start_two = 5; // so the lines are offset
 
-bool *matrix;
-int max_length;
+int end_one = line_width;
+int end_two = line_width + 5; // so the lines are offset
+
+bool *window_one;
+bool *window_two;
 bool *window;
 
+bool *matrix_one;
+bool *matrix_two;
 
-void check_start_end(int max_length){
-  if (start < max_length)
-      start++;
-  else
-      start = 0;
+int max_length_one;
+int max_length_two;
 
-  if (end < max_length)
-      end++;
-  else
-      end = 0;
+int line_size;
+
+
+void check_start_end(int max_length, int num){
+  if (num == 1){
+      if (start_one < max_length)
+        start_one++;
+      else
+        start_one = 0;
+
+      if (end_one < max_length)
+        end_one++;
+      else
+        end_one = 0;
+    }
+    else if (num == 2){
+        if (start_two < max_length)
+          start_two++;
+        else
+          start_two = 0;
+
+        if (end_two < max_length)
+          end_two++;
+        else
+          end_two = 0;
+      }
 }
 
 
 void setup() {
     delay( 5000 ); // power-up safety delay
 
-    char word[] = "A B A A C";
+    // lines need to be only capital letters
+    char line_one[] = "IEEE EVERY OTHER WEEK ON TUESDAY @ 5:30P";
+    char line_two[] = "CONTACT: PRESIDENT@UTIEEECOM";
     Serial.begin(9600);
 
-    matrix = form_matrix(word, line_width, num_lines);
-    max_length = get_max_length(word, line_width);
+    matrix_one = form_matrix(line_one, line_width, num_lines / 2);
+    max_length_one = get_max_length(line_one, line_width);
+
+    matrix_two = form_matrix(line_two, line_width, num_lines / 2);
+    max_length_two = get_max_length(line_two, line_width);
+
+    line_size = line_width * (num_lines / 2);
+
+    for( int i = 0; i < num_lines / 2; i++){
+      for( int j = 0; j < max_length_one; j++){
+        Serial.print(matrix_one[i * max_length_one + j]);
+        Serial.print(" ");
+      }
+      Serial.print("\r\n");
+    }
+    Serial.print("\r\n");
+
+    for( int i = 0; i < num_lines / 2; i++){
+      for( int j = 0; j < max_length_two; j++){
+        Serial.print(matrix_two[i * max_length_two + j]);
+        Serial.print(" ");
+      }
+      Serial.print("\r\n");
+    }
+    Serial.print("\r\n");
+
+    window_one = new bool[line_size];
+    window_two = new bool[line_size];
     window = new bool[num_lines * line_width];
 
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, num_leds).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
+    clear_letters();
+    Serial.print("Loaded");
 }
 
 
 void loop(){
     // updates window
-    scroll(window, line_width, num_lines, max_length, matrix, start, end);
+    scroll(window_one, line_width, num_lines, max_length_one, matrix_one, start_one, end_one);
+    scroll(window_two, line_width, num_lines, max_length_two, matrix_two, start_two, end_two);
+    // add in line one
+    for(int i = 0; i < line_size; i++){
+        window[i] = window_one[i];
+    }
+
+    // add in line two
+    for(int i = 0; i < line_size; i++){
+        window[line_size + i] = window_two[i];
+    }
 
     Serial.println("");
 
@@ -194,7 +326,8 @@ void loop(){
       }
     }
 
-    check_start_end(max_length);
+    check_start_end(max_length_one, 1);
+    check_start_end(max_length_two, 2);
 
     Serial.println();
 
